abf55a9917e365b8cc6fe80a17571e7e3076fea2
Stefanie <stefcodes@gmail.com>
2020-05-22 14:05:44 -0700

    remove boilerplate code

--- travelVR/index.js
+++ travelVR/index.js
@@ -8,36 +8,10 @@ import {
 
 export default class travelVR extends React.Component {
   render() {
-    return (
-      <View style={styles.panel}>
-        <View style={styles.greetingBox}>
-          <Text style={styles.greeting}>
-            Welcome to React 360
-          </Text>
-        </View>
-      </View>
-    );
+    return null
   }
 };
 
-const styles = StyleSheet.create({
-  panel: {
-    // Fill the entire surface
-    width: 1000,
-    height: 600,
-    backgroundColor: 'rgba(255, 255, 255, 0.4)',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  greetingBox: {
-    padding: 20,
-    backgroundColor: '#000000',
-    borderColor: '#639dda',
-    borderWidth: 2,
-  },
-  greeting: {
-    fontSize: 30,
-  },
-});
+const styles = StyleSheet.create({});
 
 AppRegistry.registerComponent('travelVR', () => travelVR);
