2927e1d537f8f502ddba872185d10dd544f0dd4d
Stefanie <stefcodes@gmail.com>
2020-05-22 14:49:13 -0700

    align names center

--- travelVR/index.js
+++ travelVR/index.js
@@ -1,17 +1,40 @@
 import React from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View,
-} from 'react-360';
+import {AppRegistry, StyleSheet, Text, View } from 'react-360';
 
 export default class travelVR extends React.Component {
   render() {
-    return null
+    return (
+      <View style={styles.mainView}>
+        <Text style = {styles.menuItem}> Shamika</Text>
+        <Text style = {styles.menuItem}> Kiesha</Text>
+        <Text style = {styles.menuItem}> Tara</Text>
+        <Text style = {styles.menuItem}> Shonda</Text>
+        <Text style = {styles.menuItem}> Sabrina</Text>
+        <Text style = {styles.menuItem}> Crystal</Text>
+        <Text style = {styles.menuItem}> Derhonda</Text>
+      </View>
+    );
   }
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  mainView: {
+    height: 600, 
+    width: 600, 
+    padding: 10, 
+    backgroundColor: '#eee',
+    alignItems: 'center', 
+    justifyContent:'center'
+  }, 
+  menuItem: {
+    fontSize: 40, 
+    width: '50%', 
+    marginTop: 5, 
+    backgroundColor: '#0298D0',
+    color: 'white'
+
+  }
+
+});
 
 AppRegistry.registerComponent('travelVR', () => travelVR);
